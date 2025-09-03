f7545bfbf69f66daf631676736fc6ede38451d7f
Iganc <145295755+Iganc@users.noreply.github.com>
2025-09-03 21:13:18 +0200

    Update knex configuration to include SSL option

--- app.js
+++ app.js
@@ -18,14 +18,15 @@ app.locals.formatDuration = function(seconds) {
 };
 require('dotenv').config();
 
-const db = knex({
+const db = require('knex')({
   client: 'pg',
   connection: {
     host: process.env.DB_HOST,
     port: process.env.DB_PORT,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME
+    database: process.env.DB_NAME,
+    ssl: { rejectUnauthorized: false }
   }
 });
 
